4c75d02f507b37f6c650f5949f6ccc653a65d37f
Nathan Grey <nathan@eta.email>
2021-02-22 22:29:36 -0800

    js wip

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
 
   $( "#new-pic" ).click(function() {
+    var ids;
+    var id;
+    
     $.get("https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&hasImages=true&departmentIds=3|5|6|7|9|10|11|12|13|14|17|21q=*", function( data ) {
-      var ids = data["objectIDs"];
-      var id = ids[Math.floor(Math.random() * ids.length)];
-      $.get( `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`, function( data ) {
-        $('#met-image').attr('src', data["primaryImageSmall"]);
-        $('#met-link').attr('href', data["objectURL"]);
-        $('#caption').hide().text(data["title"]).show('slow');
-      });
+      ids = data["objectIDs"];
+      id = ids[Math.floor(Math.random() * ids.length)];
     });
+      
+    $.get( `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`, function( data ) {
+      $('#met-image').attr('src', data["primaryImageSmall"]);
+      $('#met-link').attr('href', data["objectURL"]);
+      $('#caption').hide().text(data["title"]).show('slow');
+    });
+      
   });
+  
 }); 
