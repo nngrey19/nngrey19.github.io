205b090862cd8117d046e6179cafa4e07577fd69
Nathan Grey <nathan@eta.email>
2021-02-22 22:49:00 -0800

    js wip

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,19 +11,18 @@ $(document).ready(function(){
         var title = data["title"];
         var url = data["objectURL"];
         updateImage(img).done(updateLinkAndCaption(title, url));
+        
+        var updateImage = function(img) {
+          $('#met-image').attr('src', img);
+        };
+        
+        var updateLinkAndCaption = function(title, url) {
+          $('#caption').text(title);
+          $('#met-link').attr('href', url);
+        };
       });
     });
     
   });
   
 }); 
-
-
-var updateImage = function(img) {
-  $('#met-image').attr('src', img);
-};
-
-var updateLinkAndCaption = function(title, url) {
-  $('#caption').text(title);
-  $('#met-link').attr('href', url);
-};
